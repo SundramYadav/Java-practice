59cd2a2950e707549a48335e633f3edbf0512f5a
SundramYadav <108608052+SundramYadav@users.noreply.github.com>
2026-04-02 05:10:57 +0000

    ..

--- my_react_app/src/App.jsx
+++ my_react_app/src/App.jsx
@@ -7,10 +7,21 @@ import New from './component/assignment'
 function App() {
   const [count, setCount] = useState(0)
 
+  function fun(){
+    alert ("alert")
+  }
+
+  const handleClick=(fruitName)=>{
+    alert ("fruitname")
+  }
+
   return (
     <>
-    
-      <New></New>
+    <h1> Example of click event </h1>
+    <button onClick={fun}>click</button>
+   <button onClick={() => handleClick("Apple")}>APPLE</button>
+      <button onClick={() => handleClick("Banana")}>BANANA</button>
+      <button onClick={() => handleClick("Guava")}>GUAVA</button>
     </>
   )
 }
